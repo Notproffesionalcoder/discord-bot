9c85d8ebb59b8d8f3deb72dd85b31e27914be43c
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2021-04-20 23:39:48 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,8 @@ TOKEN = os.getenv("TOKEN")
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('!test'))
-    print("Logged in as Bot")
+    print(f"Logged in as {bot.user.name}")
+    print(f"Bot ID: {bot.user.id}")
     
 @client.command()
 async def test(ctx):
