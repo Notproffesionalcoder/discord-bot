2a7e9ebccd565bf911b663bafa30cd2c5b91e5e1
TheYoBots <elton_nathan@yahoo.com>
2021-04-20 19:28:24 +0000

    Add ping pong emoji

--- bot/main.py
+++ bot/main.py
@@ -25,7 +25,7 @@ async def test(ctx):
     
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'{round(client.latency * 1000)}ms')
+    await ctx.send(f"{round(client.latency * 1000)}ms 🏓")
 
 if __name__ == "__main__":
     client.run(TOKEN)
