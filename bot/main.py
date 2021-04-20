1b176e265cb1d03af9c16c4fa8fd34ab7c03fac7
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2021-04-20 23:01:30 +0530

    bot to client

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,16 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("TOKEN")
 
-@bot.event
+@client.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.active, activity=discord.Listening("!test"))
+    await client.change_presence(status=discord.Status.active, activity=discord.Listening("!test"))
     print(f"Logged in as {bot.user.name}")
     print(f"Bot ID: {bot.user.id})")
 
-@bot.command()
+@client.command()
 async def test(ctx):
     await ctx.send("Tested! It works!")
 
