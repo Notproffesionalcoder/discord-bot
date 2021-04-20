a871704050b18600400d08f89d19b0bd300b2b03
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2021-04-20 23:17:50 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,17 +3,17 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("TOKEN")
-
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.active, activity=discord.Game("!test"))
-    print("Custom Status Activated")
     
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user.name}")
     print(f"Bot ID: {client.user.id})")
 
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.active, activity=discord.Game("!test"))
+    print("Custom Status Activated")
+    
 @client.command()
 async def test(ctx):
     await ctx.send("Tested! It works!")
