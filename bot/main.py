d8ff366a118d93845c64d6eb5780a8cb79234582
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2021-04-21 00:33:36 +0530

    Add command

--- bot/main.py
+++ bot/main.py
@@ -7,13 +7,17 @@ TOKEN = os.getenv("TOKEN")
     
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name='!support'))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name='!support | By TheYoBots | https://github.com/TheYoBots/discord-bot'))
     print(f"Logged in as {client.user.name}")
     print(f"Bot ID: {client.user.id}")
     
 @client.command()
 async def support(ctx):
-    await ctx.send("List of commands: `!test`")
+    await ctx.send("List of commands: `!test`, `!source`")
+    
+@client.command()
+async def source(ctx):
+    await ctx.send("This Bot is made by TheYoBots. Checkout the source code here: https://github.com/TheYoBots/discord-bot")
     
 @client.command()
 async def test(ctx):
