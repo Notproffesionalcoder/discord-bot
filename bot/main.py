db169c6ea796683a1606edc4757b8d79d62d522c
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2021-04-21 13:33:44 +0530

    Add !guessthenumber

--- bot/main.py
+++ bot/main.py
@@ -15,7 +15,7 @@ async def on_ready():
     
 @client.command()
 async def support(ctx):
-    await ctx.send("List of commands: `!test`, `!source`, `!say <value>`, `!ping`, `!meme`")
+    await ctx.send("List of commands: `!test`, `!source`, `!say <value>`, `!ping`, `!meme`, `!guessthenumber`")
     
 @client.command()
 async def source(ctx):
@@ -43,5 +43,20 @@ async def meme(ctx):
             embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
             await ctx.send(embed=embed)
 
+@client.command()
+async def guessthenumber(ctx):
+    computer = random.randint(1, 10)
+    await ctx.send("Guess my number. It could be any from 1 to 10.")
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel and int(msg.content) in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    msg = await client.wait_for("message", check=check)
+
+    if int(msg.content) == computer:
+        await ctx.send("Correct")
+    else:
+        await ctx.send(f"Nope it was {computer}")
+            
 if __name__ == "__main__":
     client.run(TOKEN)
