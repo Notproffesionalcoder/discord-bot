46342795d03e2362a62353ec68b9c63b0b427a02
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-08-23 17:01:35 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,12 +4,12 @@ import random
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix=".")
 TOKEN = os.getenv("TOKEN")
     
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name='!support | By ~Rishi#0100'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name='.support | By ~Rishi#0100'))
     print(f"Logged in as {client.user.name}")
     print(f"Bot ID: {client.user.id}")
     
